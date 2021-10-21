6981264eb10beb6707786eb3efbccd8ed86f6eab
OvidijusV <ovidijusv14@gmail.com>
2021-10-21 16:20:09 +0300

    init, added hash

---
+++ declarations.h
@@ -0,0 +1,23 @@
+#ifndef DELCARATIONS_H
+#define DELCARATIONS_H
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <bitset>
+#include <math.h>
+#include <ctime>
+#include <chrono>
+using namespace std;
+
+void checkChoice(int &choice);
+int countWords(string str);
+string hashFunction(string inputHash);
+
+
+
+
+#endif
