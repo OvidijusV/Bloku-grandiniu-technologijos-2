b2ade21970b5569607dd5f9dc5149c910b7b4317
OvidijusV <ovidijusv14@gmail.com>
2021-10-21 17:34:33 +0300

    created transaction header

---
+++ Transaction.hpp
@@ -0,0 +1,23 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include "declarations.h"
+#include "User.hpp"
+
+class Transaction {
+    public:
+    Transaction(User* sender, User* receiver, int amount);
+    string transactionId;
+    User* sender;
+    User* receiver;
+    int amount;
+};
+
+Transaction::Transaction(User* sender, User* receiver, int amount){
+    this->sender = sender;
+        this->receiver = receiver;
+        this->amount = amount;
+        this->transactionId = hashFunction(sender->getKey() + receiver->getKey() + to_string(amount));
+}
+
+#endif
